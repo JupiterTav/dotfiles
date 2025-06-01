82f20a4681338695d111c503daf71758c5af6177
JupiterTav <wesleyTavaresDev@outlook.com>
2025-06-01 16:54:00 -0300

    neotree

---
+++ nvim/lua/plugins/neotree.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  lazy = false, -- neo-tree will lazily load itself
+  opts = {
+  },
+}
