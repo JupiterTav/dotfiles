e4a182d0c828105ed201bfb67ef3a5008843d1c3
JupiterTav <wesleyTavaresDev@outlook.com>
2026-08-04 16:20:14 +0000

    wallpapers

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -70,6 +70,14 @@ return {
             treesitter = {'lsp'},
             columns = {{ "label", "label_description", gap = 1}, {"kind_icon"}},
           components = {
+               label = {
+                    text = function(ctx)
+                        return require("colorful-menu").blink_components_text(ctx)
+                    end,
+                    highlight = function(ctx)
+                         return require("colorful-menu").blink_components_highlight(ctx)
+                    end,
+                },
 
             kind_icon = {
               text = function(ctx)
